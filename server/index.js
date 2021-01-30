5d7a61ae98245f356082cbbe6105b77b738b3724
Ana Tasevska <tasevska.anna@yahoo.com>
2021-01-30 18:58:30 +0100

    'makeitbeter'

--- server/index.js
+++ server/index.js
@@ -2,27 +2,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
 import postRoutes from './routes/posts.js';
-import userRoutes from './routes/users.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
+import userRouter from "./routes/users.js";
 
 const app = express();
-
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-app.use("/user", userRoutes);
-//const uri =  'mongodb+srv://admin:Reserveit5*@tables.i4uiv.mongodb.net/tables?retryWrites=true&w=majority'
+app.use("/user", userRouter);
+const CONNECTION_URL =  'mongodb+srv://admin:Reserveit5*@tables.i4uiv.mongodb.net/tables?retryWrites=true&w=majority'
 
 
-mongoose.connect(process.env.CONNENCTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(process.env.PORT || 5000, () => console.log(`Server Running`)))
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
